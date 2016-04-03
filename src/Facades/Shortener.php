4927755103eb94582b3cf4ad978e6d43d2bafbd7
Tyler Arbon <tylercd100@gmail.com>
2016-04-03 17:17:58 -0600

    Added Facade

---
+++ src/Facades/Shortener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zenapply\Shortener\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Illuminate\Filesystem\Filesystem
+ */
+class Shortener extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'shortener';
+    }
+}
